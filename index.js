20c35d3f10589af5e3473fbcc486a7294e893009
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-06 22:04:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction() {
   return function() {
-    return theFunk
+    return theFunk;
   }
 }
 funkyFunction()
